e3e58ceb81e4b3f1d89c6fb52d333dcaaa3b3448
Ubuntu <kun_duc123@yahoo.com.vn>
2021-07-28 08:27:40 +0700

    Chapter 3+4+5: Static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    locale = params[:locale].to_s.strip.to_sym
+    check = I18n.available_locales.include?(locale)
+    I18n.locale = check ? locale : I18n.default_locale
+  end
 end
