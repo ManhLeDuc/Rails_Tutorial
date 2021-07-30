204e45ea392ab17b6e2c08fe760a679a0b740be1
Ubuntu <kun_duc123@yahoo.com.vn>
2021-07-30 15:11:08 +0700

    Chapter 7: Sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_rescue
+
+  private
   def set_locale
     locale = params[:locale].to_s.strip.to_sym
     check = I18n.available_locales.include?(locale)
     I18n.locale = check ? locale : I18n.default_locale
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
+
+  def record_not_found_rescue
+    flash[:danger] = t ".not_found"
+    redirect_to root_path
   end
 end
